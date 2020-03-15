65e1ff38b92af13792df1c6c3a5c9544130df8bf
Lennert <Lennert.berkhan@gmail.com>
2020-03-15 09:39:38 +0100

    Projektdateien hinzufügen.

---
+++ HowTo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HowTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example how to use it");
+            HowToUseIt useIt = new HowToUseIt();
+
+            for (int i = 0; i < 10; i++)
+            {
+                useIt.addOne();
+            }
+        }
+    }
+
+    class HowToUseIt
+    {
+        int number = 0;
+
+        public void addOne()
+        {
+            Console.Write("Changed number from " + number);
+            number++;
+            Console.WriteLine(" to " + number);
+        }
+    }
+}
