ad82f75f6773f839ee5a8c05b87dea5100efef43
Lennert <Lennert.berkhan@gmail.com>
2020-03-15 12:24:06 +0100

    Quellcodestand Beispielprogramm

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using OclAspectTest;
 using HowTo;
 
 namespace Test
@@ -9,12 +8,7 @@ namespace Test
 
         [Test]
         public void Test1()
-        {
-            string ocls = "context HowToUseIt::addOne() post NumberLowerThree: self.number < 3";
-
-            //Attach OCLs to program (Bool para 1. debugger 2. custom methode)
-            OclTestProvider.AddConstraints(new[] { "HowTo" }, ocls, false, false);
-
+        {     
             var program = new Program();
             Assert.IsTrue(program.countToTen());
         }
